4dcd665a4a5e61a648b8948b1d1f92eb340bf35a
Sean <sean@sean.taipei>
2018-07-06 00:12:16 +0800

    Add Fixed Nav

--- js/common.js
+++ js/common.js
@@ -65,6 +65,19 @@ input.addEventListener("keydown", function(e) {
     }
 });
 
+window.addEventListener("scroll", function () {
+    var nav = document.getElementsByTagName("nav")[0];
+    var body = document.getElementsByTagName("body")[0];
+    if (window.scrollY > 200) {
+        nav.classList.add("fixed");
+        body.style.top = "40px";
+    }
+    if (window.scrollY < 20) {
+        nav.classList.remove("fixed");
+        body.style.top = "0px";
+    }
+});
+
 function addActive(x) {
     if (!x)
         return false;
