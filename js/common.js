3a14b17497e8f88ba42ff25547b0d9a1e5876cfa
Sean Wei <me@sean.taipei>
2019-10-22 15:15:48 +0800

    Add Introduce Page

--- js/common.js
+++ js/common.js
@@ -20,8 +20,31 @@ var filterGsat = {
 	"社會": 6,
 	"自然": 6,
 }
-var subjectsAdv = Object.keys(filterAdv);
 var subjectsGsat = Object.keys(filterGsat);
+
+var filterAdv = {
+	"國文": 0,
+	"英文": 0,
+	"數學": 0,
+	"社會": 0,
+	"自然": 0
+};
+if (gsatType == "advanced")
+	var filterAdv = {
+		"國文": 0,
+		"英文": 0,
+		"數甲": 0,
+		"數乙": 0,
+		"物理": 0,
+		"化學": 0,
+		"生物": 0,
+		"歷史": 0,
+		"地理": 0,
+		"公民": 0
+	};
+
+var subjectsAdv = Object.keys(filterAdv);
+
 
 const CJK = new RegExp('[a-zA-Z\u2e80-\u2eff\u2f00-\u2fdf\u3040-\u309f\u30a0-\u30fa\u30fc-\u30ff\u3100-\u312f\u3200-\u32ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff]'); // from pangu, CJK + Alphabet
 
