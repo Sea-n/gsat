a36a91a2055c9482d87e56ca9bfd94ebf6170d2f
Sean Wei <me@sean.taipei>
2019-10-22 10:58:39 +0800

    Add CSY

--- static.php
+++ static.php
@@ -7,6 +7,7 @@ if ($path !== 'apply'
 	exit("ERROR: $uri");
 
 $file = file_get_contents("$path.html");
-$table = file_get_contents("table-$path.html");
+$table = '<h3>感謝 <a target="_blank" href="https://csy.教我.tw/">資安神童 CSY 陳思羽</a> 提供精神糧食</h3>';
+$table .= file_get_contents("table-$path.html");
 
 echo str_replace('<table class="ts very basic table" id="list"></table>', $table, $file);
