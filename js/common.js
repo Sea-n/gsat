4852aede9dbaa52e4e995adba3f5cb90a1c01e04
Sean Wei <me@sean.taipei>
2019-11-16 22:09:02 +0800

    Add Countdown for Advanced

--- js/common.js
+++ js/common.js
@@ -48,10 +48,15 @@ var subjectsAdv = Object.keys(filterAdv);
 var starResults = {};
 
 
-/* Countdown */
-var countdown = document.getElementById("countdown");
-if (countdown !== null)
-    countdown.innerHTML = Math.ceil((1579190400000 - new Date().getTime()) / 1000 / 60 / 60 / 24);
+/* Countdown GSAT */
+var countdownG = document.getElementById("countdownG");
+if (countdownG !== null)
+    countdownG.innerHTML = Math.ceil((1579190400000 - new Date().getTime()) / 1000 / 60 / 60 / 24);
+
+/* Countdown Advanced */
+var countdownA = document.getElementById("countdownA");
+if (countdownA !== null)
+    countdownA.innerHTML = Math.ceil((1593532800000 - new Date().getTime()) / 1000 / 60 / 60 / 24);
 
 
 const CJK = new RegExp('[a-zA-Z\u2e80-\u2eff\u2f00-\u2fdf\u3040-\u309f\u30a0-\u30fa\u30fc-\u30ff\u3100-\u312f\u3200-\u32ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff]'); // from pangu, CJK + Alphabet
