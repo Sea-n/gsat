ef62081e35a101095dd9c32f91218dd13d474d1a
Sean Wei <me@sean.taipei>
2020-02-25 17:43:32 +0800

    Update Advanced date

--- js/common.js
+++ js/common.js
@@ -56,7 +56,7 @@ if (countdownG !== null)
 /* Countdown Advanced */
 var countdownA = document.getElementById("countdownA");
 if (countdownA !== null)
-    countdownA.innerHTML = Math.ceil((1593532800000 - new Date().getTime()) / 1000 / 60 / 60 / 24);
+    countdownA.innerHTML = Math.ceil((1593705600000 - new Date().getTime()) / 1000 / 60 / 60 / 24);
 
 
 const CJK = new RegExp('[a-zA-Z\u2e80-\u2eff\u2f00-\u2fdf\u3040-\u309f\u30a0-\u30fa\u30fc-\u30ff\u3100-\u312f\u3200-\u32ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff]'); // from pangu, CJK + Alphabet
